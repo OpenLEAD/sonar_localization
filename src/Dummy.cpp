af7879b7848d7c0fc57b84e0962d71f6d1886784
alcantara09 <alcantara@poli.ufrj.br>
2016-02-24 13:32:19 -0300

    Initial commit

---
+++ src/Dummy.cpp
@@ -0,0 +1,10 @@
+#include "Dummy.hpp"
+#include <iostream>
+
+using namespace std;
+using namespace imaging_sonar_localization;
+
+void DummyClass::welcome()
+{
+    cout << "You successfully compiled and executed DummyProject. Welcome!" << endl;
+}
---
+++ src/Main.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <imaging_sonar_localization/Dummy.hpp>
+
+int main(int argc, char** argv)
+{
+    imaging_sonar_localization::DummyClass dummyClass;
+    dummyClass.welcome();
+
+    return 0;
+}
